efb9ebf12e36bb45d0064ba95d29914f414119c1
miguiSheep <rodriguezmiguelc.a.com>
2026-03-23 11:22:00 -0400

    new migration DTOsAdded

---
+++ ProjectHealthAPI/Migrations/20260323152119_DTOsAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectHealthAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DTOsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Ansioloiticos",
+                table: "HistoriasMedicas",
+                newName: "Ansioliticos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Ansioliticos",
+                table: "HistoriasMedicas",
+                newName: "Ansioloiticos");
+        }
+    }
+}
